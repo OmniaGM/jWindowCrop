1cc3d141b1035ec873337840256b8a593c97bc3b
Ihab Khattab <ikhattab@users.noreply.github.com>
2013-12-03 16:01:02 +0200

    Update jquery.jWindowCrop.js

--- jquery.jWindowCrop.js
+++ jquery.jWindowCrop.js
@@ -28,7 +28,7 @@
 			base.options = $.extend({},$.jWindowCrop.defaultOptions, options);
 			if(base.options.zoomSteps < 2) base.options.zoomSteps = 2;
 
-			base.$image.addClass('jwc_image').wrap('<div class="jwc_frame img-circle" />'); // wrap image in frame
+			base.$image.addClass('jwc_image').wrap('<div class="jwc_frame" />'); // wrap image in frame
 			base.$frame = base.$image.parent();
 			base.$frame.append('<div class="jwc_loader">' + base.options.loadingText + '</div>');
       base.$frame.after('<div class="jwc_controls" style="display:'+(base.options.showControlsOnStart ? 'block' : 'none')+';"><span>click to drag</span><a href="#" class="jwc_zoom_in"></a><a href="#" class="jwc_zoom_out"></a></div>');
